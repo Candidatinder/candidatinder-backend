90db58f2fb97607e947323256e05a2d9a8c3d764
alexsteeve <alexsteeve@gmail.com>
2021-10-21 17:06:44 -0300

    inclui todos os verbos para partidos

--- src/partidos/partidos.controller.ts
+++ src/partidos/partidos.controller.ts
@@ -1,9 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 @Controller('partidos')
 export class PartidosController {
   @Get()
   obterTodos(): string {
     return 'retorna todos os partidos';
+  }
+  @Get(':id')
+  obterUm(@Param() params): string {
+    return `Retorna os dados do partido ${params.id}`;
+  }
+  @Post()
+  criar(@Body() partido): string {
+    return 'Partido criado';
+  }
+  @Put()
+  atualizar(@Body() partido): string {
+    return 'Partido atualizado';
+  }
+  @Delete(':id')
+  apagar(@Param() params): string {
+    return `apaga o partido ${params.id}`;
   }
 }
